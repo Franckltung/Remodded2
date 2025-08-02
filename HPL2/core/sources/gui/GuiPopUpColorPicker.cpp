a9f3c4438b929d49bc3c60b0c2d481ef4b9d95de
NB-NutBoi <64217453+NB-NutBoi@users.noreply.github.com>
2025-08-02 04:17:16 +0200

    Whoops

--- HPL2/core/sources/gui/GuiPopUpColorPicker.cpp
+++ HPL2/core/sources/gui/GuiPopUpColorPicker.cpp
@@ -1326,7 +1326,7 @@ namespace hpl {
 		mpFCurrentColor = mpSet->CreateWidgetFrame(vPos,50,true,mpWindow);
 		mpFCurrentColor->AddCallback(eGuiMessage_OnDraw, this, kGuiCallback(ColorFrame_OnDraw));
 		mpFCurrentColor->SetDrawBackground(true);
-		mpFPreviousColor->ChangeBackgroundForColorPicking();
+		mpFCurrentColor->ChangeBackgroundForColorPicking();
 		mpFCurrentColor->SetBackgroundZ(0.1f);
 		
 		vPos.x += 25;
